93c408ad9a8d778b5542ca54f9a432eaa6ad7199
codeboysummer <aw.wahid13@gmail.com>
2022-10-22 16:01:08 -0400

    ''

---
+++ comps/navbar.js
@@ -0,0 +1,54 @@
+import { Box, Button, Flex, Heading, Image } from "@chakra-ui/react";
+import Link from "next/link";
+import {signOut} from 'firebase/auth'
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+import { auth } from "../lib/firebase";
+
+export default function Navbar() {
+ const {user,username}= useContext(UserContext)
+  return (
+    <>
+      <Flex
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        w={"100%"}
+        h={"3rem"}
+        pos={"relative"}
+        top={0}
+        left={0}
+        p={3}
+        
+      >
+        <Flex>
+          <Image />
+          <Heading color={'#f7a110 '} >Hot</Heading>
+          <Heading color={'#3ca9e9'}>Takes</Heading>
+        </Flex>
+
+        <Flex w={'30%'} alignItems={'center'}
+        justifyContent={!username?'flex-end':'space-around'}>
+          {!username && (
+            <>
+              <Link href={"/enter"}>
+                <Button colorScheme={'blue'}>Login</Button>
+              </Link>
+            </>
+          )}
+          {username && (
+            <>
+              <Link href={'/admin/takes'} >
+                <Button>Create Takes</Button>
+
+              </Link>
+              <Link href={`/${username}`}><Image cursor={'pointer'} borderRadius={'50%'} w={10} src={user?.photoURL}/></Link>
+              <Link href={'/'}>
+                <Button onClick={()=>signOut(auth)} colorScheme={"purple"} type="ghost">Logout</Button>
+              </Link>
+            </>
+          )}
+        </Flex>
+      </Flex>
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
-
+// pages/_app.js
+import { ChakraProvider } from "@chakra-ui/react";
+import {useUserData} from '../lib/hooks'
+import Navbar from "../comps/navbar";
+import { UserContext } from "../lib/context";
+import { db,auth } from "../lib/firebase";
+import { useState,useEffect } from "react";
+import {doc,collection, onSnapshot} from 'firebase/firestore'
+import {useAuthState} from 'react-firebase-hooks/auth'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user] =useAuthState(auth)
+  const [username, setusername] = useState(null)
+ 
+  const userData = useUserData();
+  
+  return (
+    <ChakraProvider>
+      <UserContext.Provider value={userData}>
+        <Navbar />
+        <Component {...pageProps} />
+      </UserContext.Provider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
